6d20a7068ce74f25fa8749533cfa215246754522
Lauren <halpertln@gmail.com>
2023-01-21 15:15:12 -0700

    added events to jumble200 and jumble 400

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const jumble200 = document.getElementById("jumble-200");
 function display() {
-    alert("uchco")
+    jumble200.innerHTML = "uchco";
 };
 
 jumble200.addEventListener("click", display);
+
+const jumble400 = document.getElementById("jumble-400");
+
+function display2 () {
+    jumble400.innerHTML = "bylbo";
+};
+
+jumble400.addEventListener("click", display2);
