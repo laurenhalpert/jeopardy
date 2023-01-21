cd9b377584e17a3b5efc40f828a5abe3f1ee55c0
Lauren <halpertln@gmail.com>
2023-01-21 15:22:09 -0700

    added js events for each jumble id

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const jumble200 = document.getElementById("jumble-200");
 function display() {
     jumble200.innerHTML = "uchco";
 };
-
 jumble200.addEventListener("click", display);
 
 const jumble400 = document.getElementById("jumble-400");
-
 function display2 () {
     jumble400.innerHTML = "bylbo";
 };
-
 jumble400.addEventListener("click", display2);
+
+const jumble600 = document.getElementById("jumble-600");
+function display3 () {
+    jumble600.innerHTML = "wiplol";
+}
+jumble600.addEventListener("click", display3);
+
+const jumble800 = document.getElementById("jumble-800");
+function display4 () {
+    jumble800.innerHTML = "reltet";
+}
+jumble800.addEventListener("click", display4);
+
+const jumble1000 = document.getElementById("jumble-1000");
+function display5 () {
+    jumble1000.innerHTML = "nocitan"
+}
+jumble1000.addEventListener("click", display5);
